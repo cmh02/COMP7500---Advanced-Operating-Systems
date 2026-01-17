c7ca126f2e9408edab3b1ca7db4d55f40ab3965b
cmh02 <cmh0201@auburn.edu>
2026-01-17 16:48:50 -0600

    Adding new reader module

---
+++ Project 2 - pWordCount/src/pwc_reader.h
@@ -0,0 +1,23 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: pwc_reader.h
+	Project: Project 2 - pWordCount
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the reader module for pWordCount.
+
+	--------------------------------------------------
+
+	## References
+	
+	1.
+
+	--------------------------------------------------
+*/
