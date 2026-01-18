bc92e4dcf73ab46e7fa82dee312a1f2421c12885
cmh02 <cmh0201@auburn.edu>
2026-01-18 16:43:25 -0600

    Initial file reader process implementation

--- Project 2 - pWordCount/src/pwc_reader.c
+++ Project 2 - pWordCount/src/pwc_reader.c
@@ -17,11 +17,80 @@
 
 	## References
 	
-	1.
+	1. https://www.geeksforgeeks.org/c/input-output-system-calls-c-create-open-close-read-write/
+	-> I used this GeeksForGeeks article to further understand behavior of open(), read(), and write() calls.
+
 
 	--------------------------------------------------
 */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include "pwc_utils.h"
+#define FILE_READ_BUFFER_SIZE 4096
+
 int pwc_reader_streamFileToPipe(const char* filePath, int writePipeFileDescriptor, int readPipeFileDescriptor) {
-	return 0;
+	
+	// Open the file in read-only mode
+	int textFileDescriptor = open(filePath, O_RDONLY);
+
+	// Confirm that the file was opened successfully
+	if (textFileDescriptor < 0) {
+		pwc_errorWithPrefix("The attempt to open the specified file has failed!");
+		return -1;
+	}
+
+	// Ceate a buffer to hold chunks of text from file
+	char textReadingBuffer[FILE_READ_BUFFER_SIZE];
+
+	// Keep reading from the file in chunks until end of file is reached
+	ssize_t numberBytesRead;
+	while ((numberBytesRead = read(textFileDescriptor, textReadingBuffer, FILE_READ_BUFFER_SIZE)) > 0) {
+
+		// Write the bytes to the pipe
+		ssize_t numberBytesWritten = write(writePipeFileDescriptor, textReadingBuffer, numberBytesRead);
+
+		// Confirm that all bytes were written to the pipe
+		if (numberBytesWritten != numberBytesRead) {
+			pwc_errorWithPrefix("The attempt to write data to the pipe has failed!");
+			close(writePipeFileDescriptor);
+			close(readPipeFileDescriptor);
+			close(textFileDescriptor);
+			return -1;
+		}
+	}
+
+	// Check for any errors while reading from the file
+	if (numberBytesRead < 0) {
+		pwc_errorWithPrefix("The attempt to read data from the file has failed!");
+		close(writePipeFileDescriptor);
+		close(readPipeFileDescriptor);
+		close(textFileDescriptor);
+		return -1;
+	}
+
+	// Close the write end of the pipe to signal end of data for the counter
+	close(writePipeFileDescriptor);
+
+	// Read the final count from the counter via the read pipe
+	int finalWordCount;
+	ssize_t numberBytesReadFromPipe = read(readPipeFileDescriptor, &finalWordCount, sizeof(finalWordCount));
+
+	// Check for any errors while reading from the pipe
+	if (numberBytesReadFromPipe < 0) {
+		pwc_errorWithPrefix("The attempt to read the final word count from the pipe has failed!");
+		close(readPipeFileDescriptor);
+		close(textFileDescriptor);
+		return -1;
+	}
+
+	// Close the read end of the pipe
+	close(readPipeFileDescriptor);
+
+	// Close the file descriptor
+	close(textFileDescriptor);
+
+	// Return the final word count
+	return finalWordCount;
 }
