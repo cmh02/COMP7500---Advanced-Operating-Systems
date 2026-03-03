cbe429e6a377eb5148022b12903a50061b407dfe
cmh02 <cmh0201@auburn.edu>
2026-03-03 13:00:28 -0600

    Initializing scheduler module

---
+++ Project 3 - AUbatch/src/aubatch_scheduler.c
@@ -0,0 +1,23 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: aubatch_scheduler.c
+	Project: Project 3 - AUbatch
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the scheduler module for AUbatch.
+
+	--------------------------------------------------
+
+	## References
+	
+	1. 
+
+	--------------------------------------------------
+*/
---
+++ Project 3 - AUbatch/src/aubatch_scheduler.h
@@ -0,0 +1,23 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: aubatch_scheduler.h
+	Project: Project 3 - AUbatch
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the scheduler module for AUbatch.
+
+	--------------------------------------------------
+
+	## References
+	
+	1. 
+
+	--------------------------------------------------
+*/
