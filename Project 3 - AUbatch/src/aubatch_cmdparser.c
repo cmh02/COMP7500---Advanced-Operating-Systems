b66c1d293f148050177cf17508b2e9df17d24632
cmh02 <cmh0201@auburn.edu>
2026-03-02 23:08:57 -0600

    Initializing command parser module for aubatch

---
+++ Project 3 - AUbatch/src/aubatch_cmdparser.c
@@ -0,0 +1,23 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: aubatch_cmdparser.c
+	Project: Project 3 - AUbatch
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the command parser module for AUbatch.
+
+	--------------------------------------------------
+
+	## References
+	
+	1. 
+
+	--------------------------------------------------
+*/
---
+++ Project 3 - AUbatch/src/aubatch_cmdparser.h
@@ -0,0 +1,35 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: aubatch_cmdparser.h
+	Project: Project 3 - AUbatch
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the command parser module for AUbatch.
+
+	--------------------------------------------------
+
+	## References
+	
+	1. 
+
+	--------------------------------------------------
+*/
+
+// Prevent multiple inclusions
+#ifndef AUBATCH_CMDPARSER_H
+#define AUBATCH_CMDPARSER_H
+
+// Libraries
+
+// Project Libraries
+
+
+
+#endif // AUBATCH_CMDPARSER_H
