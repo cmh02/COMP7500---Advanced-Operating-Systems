115865a0ac373ab114fe87427eb694cb0ee0bd9f
cmh02 <cmh0201@auburn.edu>
2026-03-04 00:24:21 -0600

    Stubbing new dispatcher module

---
+++ Project 3 - AUbatch/src/aubatch_dispatcher.c
@@ -0,0 +1,23 @@
+/*
+	--------------------------------------------------
+
+	# Information
+
+	File: aubatch_dispatcher.c
+	Project: Project 3 - AUbatch
+	Author: Chris Hinkson @cmh02
+
+	--------------------------------------------------
+
+	## Description
+
+	This file provides the dispatcher module for AUbatch.
+
+	--------------------------------------------------
+
+	## References
+	
+	1. 
+
+	--------------------------------------------------
+*/
