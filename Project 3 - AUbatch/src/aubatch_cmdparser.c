276242e9ad46d9d39023e1c3f00e70f63931efc5
cmh02 <cmh0201@auburn.edu>
2026-03-04 10:47:20 -0600

    Addded priority for job submission in command handler

--- Project 3 - AUbatch/src/aubatch_cmdparser.c
+++ Project 3 - AUbatch/src/aubatch_cmdparser.c
@@ -126,31 +126,40 @@ int aubatch_cmdparser_enterCommandLoop() {
 			// Take name as second argument
 			char* jobName = args[1];
 			if (jobName == NULL) {
-				aubatch_log(AUBATCH_LOGLEVEL_ERROR, AUBATCH_MODULE_NAME, "Please provide a name when submitting a new job!");
+				aubatch_log(AUBATCH_LOGLEVEL_INTERACTIVE, AUBATCH_MODULE_NAME, "Please provide a name when submitting a new job!");
 				continue;
 			}
 			else if (strlen(jobName) >= AUBATCH_MAX_JOB_NAME_LENGTH) {
-				aubatch_log(AUBATCH_LOGLEVEL_ERROR, AUBATCH_MODULE_NAME, "Job name cannot be longer than %d characters!", AUBATCH_MAX_JOB_NAME_LENGTH - 1);
+				aubatch_log(AUBATCH_LOGLEVEL_INTERACTIVE, AUBATCH_MODULE_NAME, "Job name cannot be longer than %d characters!", AUBATCH_MAX_JOB_NAME_LENGTH - 1);
 				continue;
 			}
 
 			// Make sure that the job is an actual executable
 			if (access(jobName, X_OK) != 0) {
-				aubatch_log(AUBATCH_LOGLEVEL_ERROR, AUBATCH_MODULE_NAME, "Job '%s' is not an executable job!", jobName);
+				aubatch_log(AUBATCH_LOGLEVEL_INTERACTIVE, AUBATCH_MODULE_NAME, "Job '%s' is not an executable job!", jobName);
 				continue;
 			}
 
 			// Take execution time as third argument
 			char* executionTimeRaw = args[2];
 			if (executionTimeRaw == NULL) {
-				aubatch_log(AUBATCH_LOGLEVEL_ERROR, AUBATCH_MODULE_NAME, "Please provide an execution time when submitting a new job!");
+				aubatch_log(AUBATCH_LOGLEVEL_INTERACTIVE, AUBATCH_MODULE_NAME, "Please provide an execution time when submitting a new job!");
 				continue;
 			}
 			uint32_t executionTime;
 			aubatch_parseUnsignedInt32(executionTimeRaw, &executionTime);
 
+			// Take priority as fourth argument
+			char* priorityRaw = args[3];
+			if (priorityRaw == NULL) {
+				aubatch_log(AUBATCH_LOGLEVEL_INTERACTIVE, AUBATCH_MODULE_NAME, "Please provide a priority when submitting a new job!");
+				continue;
+			}
+			uint32_t priority;
+			aubatch_parseUnsignedInt32(priorityRaw, &priority);
+
 			// Make job and insert
-			struct aubatch_job job = aubatch_jobs_createNewJob(jobName, executionTime, 1);
+			struct aubatch_job job = aubatch_jobs_createNewJob(jobName, executionTime, priority);
 			aubatch_scheduler_insert(job);
 
 			// Print submission message
