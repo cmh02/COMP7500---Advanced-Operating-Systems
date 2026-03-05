c05e61d26726f65fc18842f4535dee16015bd258
cmh02 <cmh0201@auburn.edu>
2026-03-04 22:53:32 -0600

    Added graph analysis for workload condition metrics

--- Project 3 - AUbatch/report-data/analyzeAubatchWorkload.c
+++ Project 3 - AUbatch/report-data/analyzeAubatchWorkload.c
@@ -20,6 +20,11 @@
 
 	--------------------------------------------------
 
+	# References
+
+	1. https://stackoverflow.com/questions/34971220/echo-1-50-2-than-pipe-into-another-program-in-c
+	-> I used this post to look at running aubatch from here.
+
 */
 
 // Libraries
@@ -32,6 +37,10 @@ int main() {
 
 	// Define the target job to use in these benchmarks
 	char* targetJobName = "../jobs/job_randsleep5";
+
+	// Define the options for scheduling policy
+	char* options_schedulingPolicy[] = {"fcfs", "sjf", "priority"};
+	uint32_t numOptions_schedulingPolicy = sizeof(options_schedulingPolicy) / sizeof(options_schedulingPolicy[0]);
 
 	// Define the options for number of submitted jobs
 	uint32_t options_numSubmittedJobs[] = {5, 10, 25};
@@ -54,6 +63,36 @@ int main() {
 	double numOptions_loadDistribution = sizeof(options_loadDistribution) / sizeof(options_loadDistribution[0]);
 
 	// Loop through all combinations
-	 
+	for (int iJobs = 0; iJobs < numOptions_numSubmittedJobs; iJobs++) {
+		for (int iArrivalRate = 0; iArrivalRate < numOptions_arrivalRate; iArrivalRate++) {
+			for (int iPriorityLevels = 0; iPriorityLevels < numOptions_priorityLevels; iPriorityLevels++) {
+				for (int iLoadDistribution = 0; iLoadDistribution < numOptions_loadDistribution; iLoadDistribution++) {
+					for (int iSchedulingPolicy = 0; iSchedulingPolicy < numOptions_schedulingPolicy; iSchedulingPolicy++) {
+
+						// Print current combo being run
+						printf("Running benchmark with scheduling policy '%s', number of jobs %u, arrival rate %f, number of priority levels %u, minimum CPU time %f, and maximum CPU time %f!\n", options_schedulingPolicy[iSchedulingPolicy], options_numSubmittedJobs[iJobs], options_arrivalRate[iArrivalRate], options_priorityLevels[iPriorityLevels], options_loadDistribution[iLoadDistribution][0], options_loadDistribution[iLoadDistribution][1]);
+
+						// Build command for running aubatch
+						char command[2056];
+
+						// Copy in values
+						snprintf(command, sizeof(command), "echo \"test %s %s %d %f %d %f %f\nquit\n\" | ../build/aubatch",
+							targetJobName,
+							options_schedulingPolicy[iSchedulingPolicy],
+							options_numSubmittedJobs[iJobs],
+							options_arrivalRate[iArrivalRate],
+							options_priorityLevels[iPriorityLevels],
+							options_loadDistribution[iLoadDistribution][0],
+							options_loadDistribution[iLoadDistribution][1]
+						);
+
+						// Run command
+						system(command);
+
+					}
+				}
+			}
+		}
+	}
 
 }
